8294982dbbfd2513edc53a9011eb21097d46645d
gua-guargia <georgiazhang221@gmail.com>
2020-09-17 20:41:33 +0800

    Add Level 7 Save

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -1,22 +1,31 @@
 package duke.main;
+
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import duke.exception.DukeException;
 import duke.task.Task;
 import duke.task.Todo;
 import duke.task.Deadline;
 import duke.task.Event;
 
+
+
 public class Duke {
     //constants
     public static final int MAX_TASK = 100;
     public static final String lineSplit = "    ____________________________________________________________";
 
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) throws DukeException, IOException {
         boolean canExit = false;
         String line;
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[MAX_TASK];
         int taskAmount = 0;
+        String filePath = "data/duke.txt";
 
         //the logo and greet
         String logo = " ____        _        \n"
@@ -26,6 +35,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+
+        try {
+            taskAmount = printFileContents(filePath, tasks);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
 
         //main chat box
         while(!canExit) {
@@ -38,16 +53,28 @@ public class Duke {
                 }
             }
             else if(line.toLowerCase().equals("bye")) {
+                try {
+                    writeFile(filePath, tasks, taskAmount);
+                } catch (IOException e) {
+                    System.out.println(lineSplit);
+                    System.out.println("    Sorry, I cannot save the file");
+                    System.out.println(lineSplit);
+                }
                 canExit = true;
             }
             else if(line.toLowerCase().contains("done")) {
                 try {
                     markAsDone(line, tasks, taskAmount);
+                    writeFile(filePath, tasks, taskAmount);
                 } catch(DukeException e) {
                     e.printError();
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println(lineSplit);
                     System.out.println("    it is not a task number in your list, please enter again");
+                    System.out.println(lineSplit);
+                } catch (IOException e) {
+                    System.out.println(lineSplit);
+                    System.out.println("    Sorry, I cannot save the file");
                     System.out.println(lineSplit);
                 }
             }
@@ -56,6 +83,7 @@ public class Duke {
                     try {
                         tasks[taskAmount] = new Todo(line);
                         tasks[taskAmount].doneAddTask();
+                        tasks[taskAmount].writeFile();
                         taskAmount++;
                     } catch (DukeException e) {
                         e.printError();
@@ -63,12 +91,17 @@ public class Duke {
                         System.out.println(lineSplit);
                         System.out.println("    ☹ OOPS!!! It is not a proper todo format, please enter again");
                         System.out.println(lineSplit);
+                    } catch (IOException e) {
+                        System.out.println(lineSplit);
+                        System.out.println("    ☹ OOPS!!! Something wrong with the file, please check the /data/duke.txt");
+                        System.out.println(lineSplit);
                     }
                 }
                 else if(line.toLowerCase().contains("deadline")) {
                     try {
                         tasks[taskAmount] = new Deadline(line);
                         tasks[taskAmount].doneAddTask();
+                        tasks[taskAmount].writeFile();
                         taskAmount++;
                     } catch (DukeException e) {
                         e.printError();
@@ -76,18 +109,27 @@ public class Duke {
                         System.out.println(lineSplit);
                         System.out.println("    ☹ OOPS!!! It is not a proper deadline format, please enter again");
                         System.out.println(lineSplit);
+                    } catch (IOException e) {
+                        System.out.println(lineSplit);
+                        System.out.println("    ☹ OOPS!!! Something wrong with the file, please check the /data/duke.txt");
+                        System.out.println(lineSplit);
                     }
                 }
                 else if(line.toLowerCase().contains("event")) {
                     try {
                         tasks[taskAmount] = new Event(line);
                         tasks[taskAmount].doneAddTask();
+                        tasks[taskAmount].writeFile();
                         taskAmount++;
                     } catch (DukeException e) {
                         e.printError();
                     } catch (StringIndexOutOfBoundsException e) {
                         System.out.println(lineSplit);
                         System.out.println("    ☹ OOPS!!! It is not a proper event format, please enter again");
+                        System.out.println(lineSplit);
+                    } catch (IOException e) {
+                        System.out.println(lineSplit);
+                        System.out.println("    ☹ OOPS!!! Something wrong with the file, please check the /data/duke.txt");
                         System.out.println(lineSplit);
                     }
                 }
@@ -142,5 +184,58 @@ public class Duke {
         }
         int taskIndex = (Integer.parseInt(words[1])) - 1;
         tasks[taskIndex].markAsDone();
+    }
+
+    public static int printFileContents(String filePath, Task[] tasks) throws IOException, DukeException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        int taskAmount = 0;
+        System.out.println("    You have the following items in your list:");
+        while (s.hasNext()) {
+            //System.out.println("    " + s.nextLine());
+            String line = s.nextLine();
+            String[] words = line.split(" \\| ");
+
+            //create todo
+            if(words[0].equals("T")) {
+                tasks[taskAmount] = new Todo("todo " + words[2]);
+                if(words[1].equals("1")) {
+                    tasks[taskAmount].markAsDone();
+                }
+                taskAmount++;
+            }
+            //create deadline
+            else if(words[0].equals("D")) {
+                tasks[taskAmount] = new Deadline("deadline " + words[2] + " /by " + words[3]);
+                if(words[1].equals("1")) {
+                    tasks[taskAmount].markAsDone();
+                }
+                taskAmount++;
+            }
+            //create event
+            else if(words[0].equals("E")) {
+                tasks[taskAmount] = new Event("event " + words[2] + " /at " + words[3]);
+                if(words[1].equals("1")) {
+                    tasks[taskAmount].markAsDone();
+                }
+                taskAmount++;
+
+            }
+            else {
+                System.out.println("File reading error");
+            }
+        }
+        return taskAmount;
+    }
+
+    public static void writeFile(String filePath, Task[] tasks, int taskAmount) throws IOException {
+        //clear file
+        FileWriter fw = new FileWriter(filePath, false);
+        fw.write("");
+        fw.close();
+
+        for(int i=0; i<taskAmount; i++) {
+            tasks[i].writeFile();
+        }
     }
 }
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -2,6 +2,9 @@ package duke.task;
 import duke.main.Duke;
 import duke.exception.DukeException;
 
+import java.io.IOException;
+
+
 public abstract class Task {
     /** Number of connections to this database */
     protected String description;
@@ -9,13 +12,15 @@ public abstract class Task {
     protected String type;
     protected static int totalTasks = 0;
     public static final String NO_TYPE = "NT";
+    String filePath = "data/duke.txt";
     public static final String lineSplit = "    ____________________________________________________________";
 
 
-    public Task(String description) throws DukeException {
+    public Task(String description) throws DukeException, IOException {
         setDescription(description);
         setType();
         this.isDone = false;
+        //writeFile();
         totalTasks++;
     }
 
@@ -71,4 +76,6 @@ public abstract class Task {
         String task = getTypeIcon() + getStatusIcon() + " " + getDescription();
         return task;
     }
+
+    public abstract void writeFile() throws IOException;
 }
